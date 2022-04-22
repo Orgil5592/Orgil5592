14233fa8ff0df1af5ce12f8212bb19ea856a611e
Orgil5592 <99732626+Orgil5592@users.noreply.github.com>
2022-04-22 20:48:31 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+// function sum (a, b){
+//     return a + b+ 5;
+// }
+// console.log(sum(7, 8));
+
+
+// var a = prompt();
+// if(a >= 0)alert("eyreg");
+// else alert("sorog");
+
+// var dvn = prompt();
+// if(dvn > 100 || dvn < 0)alert('buruu utga');
+// else if(dvn >= 60)alert("tentssen");
+// else alert("tentseegui");
+
+
+// var a = prompt('a toog oruul');
+// var b = prompt('b toog oruul');
+// if(a > b){
+//     alert(a);
+//     console.log(a);
+// }
+
+// else {
+//     alert(b);
+//     console.log(b);
+// }
+
+
+
+
+
+
+
+
+// var a = prompt('a toog oruul');
+// var b = prompt('b toog oruul');
+// var c = prompt('c toog oruul');
+// function max(a, b, c){
+//     if(a > b && a>c)alert(a);
+//     else if(b>c)alert(b);
+//     else alert(c);
+// }
+// max(a, b, c);
+
+
+
+
+
+var tr = prompt('');
+if(tr % === 0)console.log("tegsh");
+else console.log("sondgoi");
+ 
+
+
+
+
+// var a = prompt('');
+// var b = prompt('');
+// var c = prompt('');
+
