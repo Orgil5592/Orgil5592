73544b0239853370941c95c42a0f3ccd8a85ffcb
Orgil5592 <99732626+Orgil5592@users.noreply.github.com>
2022-05-01 21:33:02 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,61 +1,26 @@
-// function sum (a, b){
-//     return a + b+ 5;
-// }
-// console.log(sum(7, 8));
+function calc ()
+{
+    var m1 = parseInt (document.getElementById('m1').value);
+    var m2 = parseInt (document.getElementById('m2').value);
+    var oper = document.getElementById('operators').value;
 
+    if(oper === '+')
+    {
+        var m2 = document.getElementById('result').value = m1+m2;
+    }
 
-// var a = prompt();
-// if(a >= 0)alert("eyreg");
-// else alert("sorog");
+    if(oper === '-')
+    {
+        var m2 = document.getElementById('result').value = m1-m2;
+    }
 
-// var dvn = prompt();
-// if(dvn > 100 || dvn < 0)alert('buruu utga');
-// else if(dvn >= 60)alert("tentssen");
-// else alert("tentseegui");
+    if(oper === '/')
+    {
+        var m2 = document.getElementById('result').value = m1/m2;
+    }
 
-
-// var a = prompt('a toog oruul');
-// var b = prompt('b toog oruul');
-// if(a > b){
-//     alert(a);
-//     console.log(a);
-// }
-
-// else {
-//     alert(b);
-//     console.log(b);
-// }
-
-
-
-
-
-
-
-
-// var a = prompt('a toog oruul');
-// var b = prompt('b toog oruul');
-// var c = prompt('c toog oruul');
-// function max(a, b, c){
-//     if(a > b && a>c)alert(a);
-//     else if(b>c)alert(b);
-//     else alert(c);
-// }
-// max(a, b, c);
-
-
-
-
-
-var tr = prompt('');
-if(tr % === 0)console.log("tegsh");
-else console.log("sondgoi");
- 
-
-
-
-
-// var a = prompt('');
-// var b = prompt('');
-// var c = prompt('');
-
+    if(oper === '*')
+    {
+        var m2 = document.getElementById('result').value = m1*m2;
+    }
+}
